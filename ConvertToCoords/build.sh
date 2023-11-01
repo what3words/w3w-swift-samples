6f4bf9634106502eb1ff165d8462e019dba23b0e
Thy Nguyen <>
2023-11-01 10:07:02 +0700

    [MT-791] Update build.sh

--- ConvertToCoords/build.sh
+++ ConvertToCoords/build.sh
@@ -1,1 +1,2 @@
-xcodebuild -project $folder.xcodeproj -scheme $folder -destination 'platform=OS X,arch=x86_64' PROD_API_KEY=$PROD_API_KEY build
+folder=basename $(pwd)
+xcodebuild -project $folder.xcodeproj -scheme $folder -sdk macosx -destination 'platform=OS X,arch=x86_64' PROD_API_KEY=$PROD_API_KEY build

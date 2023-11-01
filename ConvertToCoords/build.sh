62e85d3102d04c86370ca740e0064aa7884b2e15
Thy Nguyen <>
2023-11-01 13:23:37 +0700

    [MT-791] Update yml and sh file

--- ConvertToCoords/build.sh
+++ ConvertToCoords/build.sh
@@ -1,2 +1,5 @@
-folder=basename $(pwd)
-xcodebuild -project $folder.xcodeproj -scheme $folder -sdk macosx -destination 'platform=OS X,arch=x86_64' PROD_API_KEY=$PROD_API_KEY build
+for proj_file in *.xcodeproj; do
+    proj_name="${proj_file%.*}"
+    xcodebuild -resolvePackageDependencies
+    xcodebuild -project $proj_name.xcodeproj -scheme $proj_name -sdk macosx -destination 'platform=OS X,arch=x86_64' PROD_API_KEY=$PROD_API_KEY build
+done
